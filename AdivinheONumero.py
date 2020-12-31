ec76b589b0356cdcb61c43a8b7dd3073bcd61f7f
CyberHunter2 <70089448+CyberHunter2@users.noreply.github.com>
2020-12-30 23:42:06 -0300

    Add files via upload

---
+++ AdivinheONumero.py
@@ -0,0 +1,11 @@
+print("Seja Bem-Vindo!")
+
+
+print("Adivinhe o Número")
+suspeita = input("Diga um Número: ")
+adivinha = int(suspeita)
+if adivinha == 5:
+	
+	print("Parabéns, você acertou!")
+else:
+	print("Game Over")
